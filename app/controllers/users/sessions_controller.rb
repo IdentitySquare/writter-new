c254b0ce46a94c574b3172bf398799df0f6ad294
Lydia Koilparambil <47219609+LydiaKoil@users.noreply.github.com>
2023-01-25 08:26:22 +0000

    match old pr

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -12,12 +12,20 @@ class Users::SessionsController < Devise::SessionsController
   # def create
   #   super
   # end
+  
+ # DELETE /resource/sign_out
+  def destroy
+    super
+  end
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+  def after_sign_out_path_for(_resource_or_scope)
+    new_user_session_path
+  end
 
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || root_path
+  end
+  
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
