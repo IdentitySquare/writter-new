6a8778446792ed2580daf58f8517e3b35e5903ba
Lydia Koilparambil <47219609+LydiaKoil@users.noreply.github.com>
2023-01-31 14:10:09 +0000

    Tables etc

--- app/javascript/controllers/editor_controller.js
+++ app/javascript/controllers/editor_controller.js
@@ -1,5 +1,6 @@
-// import EditorJS from '@editorjs/editorjs';
+
 import { Controller } from "@hotwired/stimulus"
+// import EditorJS from 'editor.js';
 // import EditorJS from '@editorjs/editorjs';
 // import Header from '@editorjs/header';
 
@@ -17,6 +18,47 @@ export default class extends Controller {
             placeholder: 'Enter a header'
           },
         },
+        paragraph: {
+          class: Paragraph,
+          inlineToolbar: true,
+        },
+        list: {
+          class: NestedList,
+          inlineToolbar: true,
+          config: {
+            defaultStyle: 'ordered'
+          },
+        },
+        checklist: {
+          class: Checklist,
+          inlineToolbar: true,
+        },
+        Marker: {
+          class: Marker,
+        },
+        delimiter: Delimiter,
+        quote: {
+          class: Quote,
+          inlineToolbar: true,
+          config: {
+            quotePlaceholder: 'Enter a quote',
+            captionPlaceholder: 'Quote\'s author',
+          },
+        },
+        code: CodeTool,
+        inlineCode: {
+          class: InlineCode
+        },
+        underline: Underline,
+        table: {
+          class: Table,
+          inlineToolbar: true,
+          config: {
+            rows: 2,
+            cols: 3,
+          },
+        },
+
       },
 
       onChange: () => {
