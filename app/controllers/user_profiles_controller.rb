4ae8848eca5bf4964eeface7663e6f79c1867e39
Lydia Koilparambil <47219609+LydiaKoil@users.noreply.github.com>
2023-03-01 17:15:40 +0000

    form changes

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -3,9 +3,12 @@ class UserProfilesController < ApplicationController
   skip_before_action :check_onboarding
 
   def update 
-    debugger
     if @user.update(user_params)
-      redirect_to root_path
+      
+      respond_to do |format|
+        format.turbo_stream { flash.now[:notice] = "settings change" }
+        format.html {redirect_back fallback_location: root_path}
+      end
     end
   end
 
@@ -51,8 +54,17 @@ class UserProfilesController < ApplicationController
   end
 
   def user_params
-    user_params = params.require(:user).permit(:name,:username,:bio, :website,:notifications, :notifications_freq)
+    user_params = params.require(:user).permit(:name,
+                                               :username,
+                                               :bio, 
+                                               :website,
+                                               :notifications, 
+                                               :notifications_freq,
+                                               :new_article_notifications_freq,
+                                               :performance_notifications_freq,)
     user_params[:notifications_freq] = params[:user][:notifications_freq].to_i
+    user_params[:new_article_notifications_freq] = params[:user][:new_article_notifications_freq].to_i
+    user_params[:performance_notifications_freq] = params[:user][:performance_notifications_freq].to_i
     return user_params
   end
 
--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -1,27 +1,17 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-    static targets = [ "toggle", "span"]
+    static targets = [ "toggle", "span", 'form' ]
 
 
-    changeFormField(){
-    console.log('ji')
-      var was_enabled = this.toggleTarget.classList.contains('bg-emerald-800')
-
-      if (was_enabled){
-        this.toggleTarget.classList.add('bg-gray-200')
-        this.toggleTarget.classList.remove('bg-emerald-800')
-        this.spanTarget.classList.add('translate-x-5')
-        this.spanTarget.classList.remove('translate-x-0')
-      } else{
-        this.toggleTarget.classList.add('bg-emerald-800')
-        this.toggleTarget.classList.remove('bg-gray-200')
-        this.spanTarget.classList.add('translate-x-0')
-        this.spanTarget.classList.remove('translate-x-5')
-      }
-
-
-
+    submitForm(){
+      console.log('bye')
+      console.log(this.formTarget);
+      
+      this.formTarget.submit();
     }
 
+    disableFields(){
+      document.getElementById("user_notifications_freq_0").disabled = true;
+    }
 }
