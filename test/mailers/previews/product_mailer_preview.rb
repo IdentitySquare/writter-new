2d85a0195de85a81aebfa993cb9ff7bf5c3d334c
Lydia Koilparambil <47219609+LydiaKoil@users.noreply.github.com>
2023-03-12 20:10:12 +0000

    delete previews

--- test/mailers/previews/product_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/product_mailer
-class ProductMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/product_mailer/product_updates.html.haml
-  def product_updates.html.haml
-    ProductMailer.product_updates.html.haml
-  end
-
-end
