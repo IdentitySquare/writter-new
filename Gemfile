06de7da6edeaf2657546e016fa4cb5aca1f291c9
Lydia Koilparambil <47219609+LydiaKoil@users.noreply.github.com>
2023-03-17 15:08:19 +0000

    editor js image added and active storage installed

--- Gemfile
+++ Gemfile
@@ -151,3 +151,5 @@ end
 gem "factory_bot", "~> 6.2"
 
 gem "pundit", "~> 2.3"
+
+gem "image_processing", "~> 1.12"
--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -2,22 +2,25 @@
 #
 # Table name: posts
 #
-#  id           :bigint           not null, primary key
-#  body         :text
-#  draft_body   :string
-#  published_at :datetime
-#  status       :integer          default("draft"), not null
-#  title        :string
-#  created_at   :datetime         not null
-#  updated_at   :datetime         not null
-#  user_id      :bigint           not null
+#  id             :bigint           not null, primary key
+#  body           :text
+#  draft_body     :string
+#  published_at   :datetime
+#  status         :integer          default("draft"), not null
+#  title          :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  publication_id :bigint
+#  user_id        :bigint           not null
 #
 # Indexes
 #
-#  index_posts_on_user_id  (user_id)
+#  index_posts_on_publication_id  (publication_id)
+#  index_posts_on_user_id         (user_id)
 #
 # Foreign Keys
 #
+#  fk_rails_...  (publication_id => publications.id)
 #  fk_rails_...  (user_id => users.id)
 #
 require "test_helper"
