10e11a5d036f3c411794e6b696f2ba2275624c84
Lydia Koilparambil <47219609+LydiaKoil@users.noreply.github.com>
2023-03-20 08:44:01 +0000

    remove comment on delete

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -26,8 +26,7 @@ class CommentsController < ApplicationController
     @comment.discard
 
     respond_to do |format|
-      format.html { redirect_back(fallback_location: root_path)}
-      format.json { head :no_content }
+      format.turbo_stream {}
     end
   end
 
