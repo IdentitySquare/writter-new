1f13ec1c106807185297ad2313ed36bf037a65c9
Lydia Koilparambil <47219609+LydiaKoil@users.noreply.github.com>
2023-03-28 12:14:28 +0100

    rspec added

--- spec/services/emailing_service_spec.rb
+++ spec/services/emailing_service_spec.rb
@@ -1,14 +1,20 @@
   
-describe 'DailyActionReminderService' do
+describe 'EmailingService' do
 
   before do
     @user = FactoryBot.create(:user, notifications: true, new_article_notifications_freq: 0)
+    @user2 = FactoryBot.create(:user, notifications: true, new_article_notifications_freq: 0)
     @followed_user = FactoryBot.create(:user)
 
     FactoryBot.create(:follow,
       user: @user,
       followable: @followed_user)
-
+    
+    @followed_publication = FactoryBot.create(:publication)
+    FactoryBot.create(:follow,
+      user: @user,
+      followable: @followed_publication)
+  
     Timecop.freeze(2021, 9, 1, 18, 0, 0)
   end
 
@@ -18,9 +24,10 @@ describe 'DailyActionReminderService' do
 
   describe 'send_new_posts_emails_at_6pm!' do
     context 'notifications on' do
-      context 'when new posts exist' do
+      context 'when new posts exists from followed user' do
         it 'sends email when new post from follower' do
-          @post = FactoryBot.create(:post, user: @followed_user, status: 1, published_at: 1.hour.ago)
+          @post = FactoryBot.create(:post, user: @followed_user, status: 1, published_at: 1.hour.ago, publication: nil)
+        
           expect { EmailingService.new.send_new_posts_emails_at_6pm!(timezones: [@user.timezone]) }
             .to(
               have_enqueued_job.on_queue('default')
@@ -32,6 +39,29 @@ describe 'DailyActionReminderService' do
                   params: {
                     user_id: @user.id,
                     post_ids: @followed_user.posts.pluck(:id),
+                  },
+                  args: []
+                }
+              )
+            )
+        end
+      end
+
+      context 'when new posts exists from followed publication' do
+        it 'sends email when new post from follower' do
+          @post = FactoryBot.create(:post, publication: @followed_publication, user: @user2, status: 1, published_at: 1.hour.ago)
+          
+          expect { EmailingService.new.send_new_posts_emails_at_6pm!(timezones: [@user.timezone]) }
+            .to(
+              have_enqueued_job.on_queue('default')
+              .with(
+                'PostsMailer',
+                'daily_mail',
+                'deliver_now',
+                {
+                  params: {
+                    user_id: @user.id,
+                    post_ids: @followed_publication.posts.pluck(:id),
                   },
                   args: []
                 }
