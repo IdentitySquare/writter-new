4ddcb8e378cde33fe372693912b81a9e5077597b
Lydia Koilparambil <47219609+LydiaKoil@users.noreply.github.com>
2023-03-29 10:13:01 +0100

    notifiable concern fix

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -23,10 +23,15 @@
 #
 class Comment < ApplicationRecord
   include Discard::Model
+  include Notifiable
+  
   # associations
   belongs_to :user
   belongs_to :post
 
   # validations
   validates :body, length: { maximum: 1000, message: "is too long (maximum is 1000 characters)" }
+
+  # callbacks
+  
 end
--- app/models/concerns/notifiable.rb
+++ app/models/concerns/notifiable.rb
@@ -1,20 +1,32 @@
 module Notifiable
-    extend ActiveSupport::Concern
-  
-    has_many :notifications, as: :notifiable
+  extend ActiveSupport::Concern
 
-    after_create: :send_email if notifications_freq == 'instantly'
-  
-    private  
+  module ClassMethods
+    def has_notifications
+      has_many :notifications, as: :notifiable
+      # after_create :send_email, if: -> { user.notifications_freq == 'instantly' }
+      after_create :create_notification
 
-    def send_email
-      NotificationsMailer.with(mailer_params(user)).instantly_mail.deliver_now
     end
+  end
 
-    def mailer_params
-      { 
-        user_id: user.id,
-      }
-    end
+  included do
+    has_notifications
+  end
+
+  private
+
+  def create_notification
+    Notification.create(notifiable: self, user: user)
+  end
+
+  def send_email
+    NotificationsMailer.with(mailer_params(user)).instantly_mail.deliver_now
+  end
+
+  def mailer_params
+    { 
+      user_id: user.id,
+    }
+  end
 end
-  
