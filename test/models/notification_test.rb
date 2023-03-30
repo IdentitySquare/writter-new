1e2b8282ed584c4158a1f568bafdc6a52dacbc28
Lydia Koilparambil <47219609+LydiaKoil@users.noreply.github.com>
2023-03-30 11:01:01 +0100

    add sender

--- test/models/notification_test.rb
+++ test/models/notification_test.rb
@@ -9,11 +9,13 @@
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
 #  notifiable_id   :bigint           not null
+#  sender_id       :bigint
 #  user_id         :bigint           not null
 #
 # Indexes
 #
 #  index_notifications_on_notifiable  (notifiable_type,notifiable_id)
+#  index_notifications_on_sender_id   (sender_id)
 #  index_notifications_on_user_id     (user_id)
 #
 # Foreign Keys
