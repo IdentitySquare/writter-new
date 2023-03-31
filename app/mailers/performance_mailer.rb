19c619a110eb8a4ab3047feb749f7a2ca5b85e72
Lydia Koilparambil <47219609+LydiaKoil@users.noreply.github.com>
2023-03-31 19:43:03 +0100

    post views

--- app/mailers/performance_mailer.rb
+++ app/mailers/performance_mailer.rb
@@ -20,4 +20,35 @@ class PerformanceMailer < ApplicationMailer
   def set_user
     @user = User.find(params[:user_id])
   end
+
+
+  def calculate_performance(@user)
+    range = @user.notifications_frequency == 'daily' ? 1.day.ago : 1.week.ago
+
+    get_views(@user, range)
+
+  end
+
+  def get_views(@user, start_time)
+    post_ids = @user.posts.published.pluck(:id)
+    end_time = Time.now
+
+    result = ActiveRecord::Base.connection.execute("
+              SELECT properties->>'post_id' AS post_id, COUNT(*) AS visit_count
+              FROM ahoy_events
+              WHERE name = 'post viewed'
+                AND properties->>'post_id' IN ('1', '2', '3')
+                AND time BETWEEN '#{start_time}' AND '#{end_time}'
+              GROUP BY properties->>'post_id'
+            ")
+
+    @post_views = {}
+
+    result.values.each do |row|
+      @post_views[row[0]] = row[1]
+    end
+    
+    @post_views
+  end
+
 end
