3982de5b81e2613892a0de17bd0d1b26e004ce95
Lydia Koilparambil <47219609+LydiaKoil@users.noreply.github.com>
2023-05-08 12:37:28 +0100

    onboarding error fixed

--- app/javascript/controllers/onboarding_controller.js
+++ app/javascript/controllers/onboarding_controller.js
@@ -4,28 +4,33 @@ export default class extends Controller {
     static targets = [ "username", "usernameTaken", "saveButton", "errorMessage"]
 
     checkUsername(event) {
+        console.log('inher')
         const target = this.saveButtonTarget;
-        event.preventDefault()
-
+        console.log(this.usernameTarget.value.length )
         if (this.usernameTarget.value.length < 4 ) {
+            console.log('less than 4');
             console.log(this.usernameTarget.value);
             this.errorMessageTarget.classList.remove("invisible")
             this.errorMessageTarget.innerHTML = 'Username must contain atleast 4 characters'
             target.disabled = true;
-            target.classList.add("bg-disable");
-
+            target.classList.add("btn-disabled");
+            target.classList.remove("btn-primary");
             return;
         } else {
+            console.log('more than 4');
             this.errorMessageTarget.classList.add("invisible")
             target.disabled = false;
-            target.classList.remove("bg-disable");
+            target.classList.remove("btn-disabled");
+            target.classList.add("btn-primary");
         }   
 
+        const csrfToken = document.querySelector('meta[name="csrf-token"]').content;
         fetch('/onboarding/checkUsername', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
-                'Accept': 'application/json'
+                'Accept': 'application/json',
+                'X-CSRF-Token': csrfToken
             },
             body: JSON.stringify({ username: this.usernameTarget.value })
         })
@@ -36,13 +41,15 @@ export default class extends Controller {
                 this.errorMessageTarget.classList.remove("invisible")
                 this.errorMessageTarget.innerHTML = 'Username already taken'
                 target.disabled = true;
-                target.classList.add("bg-disable");
+                target.classList.add("btn-disabled");
+                target.classList.remove("btn-primary");
 
             } else {
                 
                 this.errorMessageTarget.classList.add("invisible")
                 target.disabled = false;
-                target.classList.remove("bg-disable");
+                target.classList.remove("btn-disabled");
+                target.classList.add("btn-primary");
             }
         });
 
