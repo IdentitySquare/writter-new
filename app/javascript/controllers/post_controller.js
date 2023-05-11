5cebfe4ad3d92cddbaf1b3c0c2b980c8d76a255a
Lydia Koilparambil <47219609+LydiaKoil@users.noreply.github.com>
2023-05-11 14:35:45 +0100

    fixed

--- app/javascript/controllers/post_controller.js
+++ app/javascript/controllers/post_controller.js
@@ -1,19 +1,49 @@
 import { Controller } from "@hotwired/stimulus"
 
+function debounce(func, timeout = 300){
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => { func.apply(this, args); }, timeout);
+  };
+}
+
 export default class extends Controller {
   static targets = [ "body"]
+ 
+  connect() {
+    // wait for user to stop typing
+    this.processChanges = debounce(() => this.saveInput());
 
-  saveChange() {
-    var timeoutId;
-    document.getElementById('saveStatus').innerHTML = ""
-
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(this.savePost(), 1000)
+    // handle image saving
+    // Get the Trix editor element
+    const editor = this.element.querySelector("trix-editor");
+    
+    // Listen for the trix-attachment-add event
+    editor.addEventListener("trix-attachment-add", (event) => {
+      
+      const attachment = event.attachment;
+      // If the attachment is an image, call the saveChange function
+      if (attachment.file) {
+        // wait for image to be attached (Image doesnt save if no waiting time)
+        this.timeoutID = setTimeout(() => {
+          this.saveInput();
+          console.log('save called')
+        }, 5000);
+        
+      }
+    });
   }
-
-  savePost () { 
+    
+  saveInput(){
     document.getElementById('saveStatus').innerHTML = "- Saving..."
     const postForm = document.querySelector(".simple_form")
     postForm.requestSubmit();
   }
+
+  clearStatus(){
+    console.log('ih')
+    document.getElementById('saveStatus').innerHTML = ""
+  }
+  
 }
